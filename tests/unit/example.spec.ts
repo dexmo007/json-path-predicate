7b1ef2cb03845a4f221359310a837bf3fa22c567
dexmo007 <henrik.drefs@outlook.de>
2018-11-26 20:22:53 +0100

    fixed basic test

---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,13 @@
+import {expect} from 'chai';
+import {shallowMount} from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/App.vue';
+import GitHubCorner from '@/components/GitHubCorner.vue';
+
+describe('App.vue', () => {
+  it('builds the app', () => {
+    const wrapper = shallowMount(App);
+    expect(wrapper.find(GitHubCorner)).to.exist;
+    expect(wrapper.find(HelloWorld)).to.exist;
+  });
+});
